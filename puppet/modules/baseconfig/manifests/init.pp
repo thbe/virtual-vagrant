9ffe8b16acd12979e7110344b7d09f8e306c0ccb
thbe <project@bendler-net.de>
2014-06-04 22:10:03 +0200

    Upgrade to current versions

--- puppet/modules/baseconfig/manifests/init.pp
+++ puppet/modules/baseconfig/manifests/init.pp
@@ -1,21 +1,38 @@
 # == Class: baseconfig
 #
-# Performs initial configuration tasks for all Vagrant boxes.
+# Performs initial configuration tasks for Vagrant boxes.
+# This one is only a dummy to verify the correct box setup
+# in conjunction with Puppet
+#
+# === Parameters
+#
+# === Variables
+#
+# === Examples
+#
+#  include baseconfig
+#
+# === Authors
+#
+# Thomas Bendler <project@bendler-net.de>
+#
+# === Copyright
+#
+# Copyright 2014 Thomas Bendler, unless otherwise noted.
 #
 class baseconfig {
-#  exec { 'yum -y update':
-#    command => '/usr/bin/yum -y update';
-#  }
+  #  exec { 'yum -y update':
+  #    command => '/usr/bin/yum -y update';
+  #  }
 
-#  host { 'hostmachine':
-#    ip => '192.168.0.1';
-#  }
+  #  host { 'hostmachine':
+  #    ip => '192.168.0.1';
+  #  }
 
-  file {
-    '/home/vagrant/.bashrc':
-      owner => 'vagrant',
-      group => 'vagrant',
-      mode  => '0644',
-      source => 'puppet:///modules/baseconfig/bashrc';
+  file { '/home/vagrant/.bashrc':
+    owner  => 'vagrant',
+    group  => 'vagrant',
+    mode   => '0644',
+    source => 'puppet:///modules/baseconfig/bashrc';
   }
 }
