b3afd2b18f3156fec036b4a7f077a209a63dd60f
thbe <project@bendler-net.de>
2015-01-27 01:14:02 +0100

    Updated gitignore, switched to client by default

--- centos7/script/puppet.sh
+++ centos7/script/puppet.sh
@@ -1,6 +1,10 @@
-### puppet.sh
+#! /bin/sh
 #
-# Install Puppet-Sever on minimal CentOS 7
+# This script install the puppet client or the
+# puppet server based on the script parameter
+#
+# Author: Thomas Bendler <project@bendler-net.de>
+# Date:   Tue Jan 27 00:39:54 CET 2015
 #
 
 # Get parameter
@@ -21,27 +25,35 @@ sudo yum -y update --exclude=kernel*
 # Install Puppet
 sudo yum -y install puppet
 
+# Install yum management
+sudo puppet module install thbe-yum
+sudo puppet apply -e 'class { "yum": }'
+
 if [ "x$PUPPETTYPE" = "xS" ]; then
   # Install Puppet server
   sudo yum -y install puppetserver
 
   # Install Puppet DB
-  sudo puppet module install puppetlabs/puppetdb
+  sudo puppet module install puppetlabs-puppetdb
   sudo puppet apply -e 'class { "puppetdb": }'
   sudo puppet apply -e 'class { "puppetdb::master::config": puppetdb_server => "centos.thbe.local" }'
   #sudo puppet resource package puppetdb ensure=latest
   #sudo puppet resource service puppetdb ensure=running enable=true
 
   # Install Puppet Dashboard
-  sudo puppet module install puppetlabs/mysql
-  sudo puppet module install puppetlabs/dashboard
+  sudo puppet module install puppetlabs-mysql
+  sudo puppet module install puppetlabs-dashboard
   sudo yum -y install puppet-dashboard
 fi
 
 # Install Mcollective
-sudo puppet module install puppetlabs/mcollective
+sudo puppet module install puppetlabs-mcollective
 if [ "x$PUPPETTYPE" = "xS" ]; then
   echo "Install Mcollective server"
 else
   echo "Install Mcollective client"
 fi
+
+# Install personal style
+sudo puppet module install thbe-style
+sudo puppet apply -e 'class { "style": }'
